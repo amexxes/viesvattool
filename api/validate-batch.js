b567a36f9194de194fde22fa09c70c2c1fb66461
amexxes <143995809+amexxes@users.noreply.github.com>
2026-02-20 12:21:46 +0100

    Delete api/validate-batch.js

--- api/validate-batch.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
-
-  const { vat_numbers = [], case_ref = "" } = req.body || {};
-  // TODO: plak hier je logic uit server.js (validate-batch)
-  return res.status(200).json({ ok: true });
-}
